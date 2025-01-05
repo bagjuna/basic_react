994dc095b0217d58066ecc9d678d0c71b4eeb685
juna3437@naver.com <juna3437@naver.com>
2025-01-05 15:24:34 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import Hello from "./Hello";
+import World from "./World";
+
+function App() {
+    return (
+        <div>
+            <Hello/>
+            <World/>
+            <h2>This is a test</h2>
+        </div>
+    );
+}
+
+export default App;
