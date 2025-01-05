15a6c39610e5d7423a53291a8eff849241fa7a48
juna3437@naver.com <juna3437@naver.com>
2025-01-05 15:33:58 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 
-import Hello from "./Hello";
-import World from "./World";
+import World from "./components/World";
+import Heading from "./components/Heading";
 
 function App() {
     return (
         <div>
-            <Hello/>
-            <World/>
-            <h2>This is a test</h2>
+            <Heading type="h1">Hello</Heading>
+            <Heading type="h2">World</Heading>
+
         </div>
     );
 }
