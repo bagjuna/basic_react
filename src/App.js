432ce21a1e4f543fa4e3db6756a8b5041bf34622
juna3437@naver.com <juna3437@naver.com>
2025-01-05 15:59:47 +0900

    State

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 
-import World from "./components/World";
-import Heading from "./components/Heading";
+import {useState} from "react";
 
 function App() {
+    const [value, setValue] = useState(0);
+
     return (
         <div>
-            <Heading type="h1">Hello</Heading>
-            <Heading type="h2">World</Heading>
-
+            <h1>vale: {value}</h1>
+            <button onClick={() => {
+                setValue(value + 1);
+                console.log('Increase value', value);
+            }}
+            >
+                Increase value
+            </button>
+            <button onClick={()=>{
+                setValue(0);
+            }}>
+                Reset value
+                </button>
         </div>
     );
 }
